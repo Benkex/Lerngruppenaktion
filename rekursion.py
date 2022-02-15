a808ca86dfff3eb0bfd8306b8558c2fd40075e37
Benke Hargitai <hargitai.benjoe@gmail.com>
2022-02-15 17:00:03 +0100

    .

---
+++ rekursion.py
@@ -0,0 +1,27 @@
+# ##################### REKURSION ########################
+# Was ist Rekursion?
+#   Kennst du Fraktalbilder? Was man im exercise-05 machen musste,
+# der Mandelbrot-Set war gerade ein Fraktal.
+# Das bedeutet, du kannst die gleiche Muster sehen, wenn du reinzoomst.
+#   So ist es auch bei einer rekursiven Funktion: es ruft sich selbst auf,
+# nur mit anderen Argumenten. Also quasi nochmal das ganze Bild
+# in einem Teil des Bildes.
+#   Deshalb benutzt man gerne Rekursion, um einen Fraktal zu erstellen.
+# Das werden wir schließlich auch machen. Aber erst arbeiten wir mit
+# einfacheren Rekursionen.
+
+# A1:
+# Erstelle eine Funktion mit einem Argument "n", die erst mal ihre Argument
+# ausprintet, dann sich aufruft, nur mit "n-1". Was ist deine Beobachtung?
+# Schreibe eine Bedingung dafür, so dass das letzte was ausgeprintet wird,
+# ein 0 ist! (Das ist die "Rekursionsstoppbedingung")
+
+# A2:
+# Erstelle eine Funktion mit.
+
+# A :
+# Erstell eine rekursive Funktion, die die Fakultät einer Zahl ausrechnet!
+# Wie kannst du die .
+
+def init(xs):
+    cucc = next)
