27523023b0d1eb5b7235383171fca6b4b65a7c0d
Benke Hargitai <hargitai.benjoe@gmail.com>
2022-02-15 17:38:31 +0100

    .

--- rekursion.py
+++ rekursion.py
@@ -12,16 +12,31 @@
 
 # A1:
 # Erstelle eine Funktion mit einem Argument "n", die erst mal ihre Argument
-# ausprintet, dann sich aufruft, nur mit "n-1". Was ist deine Beobachtung?
-# Schreibe eine Bedingung dafür, so dass das letzte was ausgeprintet wird,
+# ausprintet, dann sich selbst aufruft, nur mit Argument "n-1"
+# oder "n+1" (Rekursionsargument).
+#   Probier mal beide aus.
+#   Was ist deine Beobachtung?
+# Schreibe eine if-Bedingung dafür, so dass das letzte was ausgeprintet wird,
 # ein 0 ist! (Das ist die "Rekursionsstoppbedingung")
+#   Welche Rekursionsargument brauchst du, wenn das originelle "n" positiv ist?
+#   Und wenn es negativ ist? Du kannst eine if-Bedingung dafür schreiben!
 
 # A2:
-# Erstelle eine Funktion mit.
+# Erstelle eine Funktion mit einem Argument "n" (darf nicht negativ sein ->
+# wenn es trotzdem negativ ist, erzeuge ein RuntimeError!), die erst mal
+# ihre Argument ausprintet, dann als Rückkehrwert sich selbst
+# mit Argument "n-1" aufruft, falls n > 1.
+# Andernfalls soll sie 1 zurückgeben.
 
-# A :
-# Erstell eine rekursive Funktion, die die Fakultät einer Zahl ausrechnet!
-# Wie kannst du die .
+# A3:
+# Erstelle eine Funktion mit einem Argument "n", die erst mal ihre Argument
+# ausprintet, dann als Rückkehrwert die 1 + sich selbst (mit Argument n-1)
+# zurückgibt, falls n > 1.
+# Andernfalls soll sie 1 zurückgeben.
 
-def init(xs):
-    cucc = next)
+# A4:
+# Erstelle eine Funktion mit einem Argument "n", die die Zahlen 0 bis n
+# summiert!
+
+# A5:
+# Erstelle eine rekursive Funktion, die die Fakultät einer Zahl ausrechnet!
