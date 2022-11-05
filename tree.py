ac85938d8ef073c762ff1b73ad150579a3610cfc
Benkex <hargitai.benjoe@gmail.com>
2022-11-05 03:49:03 +0100

    Undo

---
+++ tree.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class BinaryNode:
+
+    def __str__(self) -> str:
+        pass
+
+    def size(self):
+        pass
+
+    def depth(self):
+        pass
+
+    def post_order(self):
+        pass
+
+    def flip(self):
+        pass
+
+
+def insert(tree: Optional[BinaryNode], id: int):
+    pass
+
+
+def create_tree(ids):
+    pass
+
+
+def find(tree, id):
+    pass
+
+
+def select(tree, lower, upper):
+    pass
+
+
+@dataclass
+class Node:
+    
+    def __str__(self):
+        pass
+
+    def weight(self):
+        pass
+
+
+def on_layer(tree, depth):
+    pass
+
+
+def leaves(tree):
+    pass
